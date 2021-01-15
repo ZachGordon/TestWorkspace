37779948c639c2321d595910ff5e8daab1776f71
gordz <gordz@ZachPC.phub.net.cable.rogers.com>
2021-01-15 11:51:02 -0500

    Commit #1

---
+++ TestProjet/src/main/java/edu/ics4u/TestProjet/HelloWorld.java
@@ -0,0 +1,10 @@
+package edu.ics4u.TestProjet;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+        System.exit(0);
+    }
+
+}
